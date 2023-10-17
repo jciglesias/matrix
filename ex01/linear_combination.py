0cb6c76199484a30d2668ac80f84e54ba25dd220
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-10-17 14:37:15 +0200

    straigth vector

--- ex01/linear_combination.py
+++ ex01/linear_combination.py
@@ -1,9 +1,9 @@
-from MatrixModule import Matrix, Vector
+from MatrixModule import Vector
 
 def linear_combination(v_lst: list, k_list: list):
     if type(v_lst) == type(k_list) and isinstance(v_lst, list) and len(v_lst) == len(k_list):
-        ret = Vector([[0] for _ in range(v_lst[0].shape[1])])
+        ret = Vector([[0 for _ in range(v_lst[0].shape[1])]])
         for row in range(len(v_lst)):
-            ret+= (v_lst[row] * k_list[row]).T()
+            ret+= v_lst[row] * k_list[row]
         return ret
     return
--- ex01/test.py
+++ ex01/test.py
@@ -1,12 +1,10 @@
-from MatrixModule import Matrix, Vector
+from MatrixModule import Vector
 from linear_combination import linear_combination
 
 if __name__=="__main__":
     v1 = Vector([[1,0,0]])
     v2 = Vector([[0,1,0]])
     v3 = Vector([[0,0,1]])
-    # m = Matrix([v1,v2,v3])
-    # m2 = Matrix([[1,2,3],[0,10,-100]])
     v4 = Vector([[1,2,3]])
     v5 = Vector([[0,10,-100]])
     e1 = [10,-2,0.5]
