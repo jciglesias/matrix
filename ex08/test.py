2b0db69c1f8bb40730b7c0cf23ffadbc38e4f6ba
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-10-20 17:41:09 +0200

    trace

---
+++ ex08/test.py
@@ -0,0 +1,9 @@
+from MatrixModule import Matrix
+
+if __name__=="__main__":
+    m = Matrix([[1, 0], [0, 1]])
+    print(m.trace())
+    m = Matrix([[2, -5, 0], [4, 3, 7], [-2, 3, 4]])
+    print(m.trace())
+    m = Matrix([[-2, -8, 4], [1, -23, 4], [0, 6, 4]])
+    print(m.trace())
