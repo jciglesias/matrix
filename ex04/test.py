bbb362024f308504d67593d1ba01d23aee03e6a0
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-11-23 17:57:51 +0100

    norm inf abs

--- ex04/test.py
+++ ex04/test.py
@@ -1,7 +1,9 @@
 from MatrixModule import Vector
 
 if __name__=="__main__":
+    v = Vector([[0,0,0]])
+    print(v.norm_1(),v.norm(),v.norm_inf())
     v = Vector([[1,2,3]])
-    print(v.norm())
-    print(v.norm_1())
-    print(v.norm_inf())
+    print(v.norm_1(),v.norm(),v.norm_inf())
+    v = Vector([[-1,-2]])
+    print(v.norm_1(),v.norm(),v.norm_inf())
