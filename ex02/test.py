6190bd6609778ac7d3aa4e529a7b92c03a33b948
Juan Carlos Iglesias <jiglesia@student.42.fr>
2023-11-23 17:43:52 +0100

    test

--- ex02/test.py
+++ ex02/test.py
@@ -2,10 +2,13 @@ from MatrixModule import Matrix, Vector
 from lerp import lerp
 
 if __name__=="__main__":
-    print(lerp(10, 20, 0.25))
-    v = Vector([[10,10,10]])
-    v2 = Vector([[20,20,20]])
-    print(lerp(v, v2, 0.25))
-    m = Matrix([[10,10],[10,10],[10,10]])
-    m2 = Matrix([[20,20],[20,20],[20,20]])
-    print(lerp(m, m2, 0.25))
+    print(lerp(0,1,0))
+    print(lerp(0,1,1))
+    print(lerp(0,1,0.5))
+    print(lerp(21,42,0.3))
+    v = Vector([[2,1]])
+    v2 = Vector([[4,2]])
+    print(lerp(v, v2, 0.3))
+    m = Matrix([[2., 1.], [3., 4.]])
+    m2 = Matrix([[20.,10.], [30., 40.]])
+    print(lerp(m, m2, 0.5))
